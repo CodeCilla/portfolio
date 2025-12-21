b2272f966b9ec05ee4595b5f2727ab049d66c843
CodeCilla <priscilla.renault72460@gmail.com>
2025-12-21 23:26:57 +0100

    fix : change cv

--- frontend/src/components/About/index.jsx
+++ frontend/src/components/About/index.jsx
@@ -11,11 +11,11 @@ const about = () => {
         <p className='about__content--text'>
           Développeuse au{' '}
           <span className='highlight'>profil hybride</span> j&apos;allie 15 ans d&apos;expérience commerciale à une solide expertise technique {' '}
-          <span className='highlight'>JavaScript, React, Python/Odoo, Java</span>. Après avoir piloté l&apos;intégration des modules complexes et d&apos;API en entreprise, je souhaite aujourd&apos;hui dépasser l&apos;exécution pour penser la {' '} 
+          <span className='highlight'>JavaScript, React, Python/Odoo, Java</span>. Après avoir piloté l&apos;intégration des modules complexes et d&apos;API en entreprise, je souhaite aujourd&apos;hui dépasser l&apos;exécution pour penser {' '} 
           <span className='highlight'> la conception globale </span> des systèmes.{' '}
           <br />
           <br />
-          Convaincue qu&apos;une bonne architecture doit servir à la rentabilité et à la stratégie, je prépare un 
+          Convaincue qu&apos;une bonne architecture doit servir à la rentabilité et à la stratégie, je souhaite prépare un 
           <span className='highlight'> {' '}Bac+5 en architecture logicielle</span>{' '}
           en alternance. Mon objectif est de concevoir des solutions scalables, valoriser vos données et aligner la technique sur vos{' '}
           <span className='highlight'>enjeux business</span>. 
