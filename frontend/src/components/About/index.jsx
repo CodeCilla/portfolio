0a02657d477aeb878c27ab00f51a697f3770d818
CodeCilla <priscilla.renault72460@gmail.com>
2026-01-05 14:53:37 +0100

    fix syntax on about

--- frontend/src/components/About/index.jsx
+++ frontend/src/components/About/index.jsx
@@ -11,14 +11,14 @@ const About = () => {
         <p className='about__content--text'>
           Développeuse au{' '}
           <span className='highlight'>profil hybride,</span> j&apos;allie 15 ans d&apos;expérience commerciale à une solide expertise technique {' '}
-          <span className='highlight'>JavaScript, React, Python/Odoo, Java</span>. Après avoir piloté l&apos;intégration des modules complexes et d&apos;API en entreprise, je souhaite aujourd&apos;hui dépasser l&apos;exécution pour penser {' '} 
+          <span className='highlight'>JavaScript, React, Python/Odoo, Java.</span> Après avoir piloté l&apos;intégration des modules complexes et d&apos;API en entreprise, je souhaite aujourd&apos;hui dépasser l&apos;exécution pour penser {' '} 
           <span className='highlight'> la conception globale </span> des systèmes.{' '}
           <br />
           <br />
           Convaincue qu&apos;une bonne architecture doit servir à la rentabilité et à la stratégie, je souhaite préparer un 
           <span className='highlight'> {' '}Bac+5 en Ingénierie logicielle</span>{' '}
           en alternance. Mon objectif est de concevoir des solutions scalables, valoriser vos données et aligner la technique sur vos{' '}
-          <span className='highlight'>enjeux business</span>. 
+          <span className='highlight'>enjeux business.</span> 
         </p>
       </div>
     </section>
