36cda483694b173f8cf1e37fa1d14f87506da2c8
CodeCilla <priscilla.renault72460@gmail.com>
2026-02-10 10:32:44 +0100

    docs: update profile focus to full-stack and data engineering paths

--- frontend/src/components/About/index.jsx
+++ frontend/src/components/About/index.jsx
@@ -6,18 +6,31 @@ const about = () => {
       <div className='border-h2'></div>
       <div className='about__content'>
         <p className='about__content--text'>
-          Hello, moi c&apos;est Priscilla, <span className='highlight'>développeuse & future experte en architecture et gestion de projet.</span>{' '}
+          Hello, moi c&apos;est Priscilla,{' '}
+          <span className='highlight'>
+            développeuse & future experte en ingénierie full-stack ou data.
+          </span>{' '}
         </p>
         <p className='about__content--text'>
-          Développeuse au{' '}
-          <span className='highlight'>profil hybride,</span> j&apos;allie 15 ans d&apos;expérience commerciale à une solide expertise technique {' '}
-          <span className='highlight'>JavaScript, React, Python/Odoo, Java.</span> Après avoir piloté l&apos;intégration des modules complexes et d&apos;API en entreprise, je souhaite aujourd&apos;hui dépasser l&apos;exécution pour penser {' '} 
-          <span className='highlight'> la conception globale </span> des systèmes.{' '}
+          Développeuse au <span className='highlight'>profil hybride,</span>{' '}
+          j&apos;allie 15 ans d&apos;expérience commerciale à une solide
+          expertise technique{' '}
+          <span className='highlight'>
+            JavaScript, React, Python/Odoo, Java.
+          </span>{' '}
+          Après avoir piloté l&apos;intégration de modules complexes et
+          d&apos;API en entreprise, je souhaite aujourd&apos;hui évoluer vers{' '}
+          <span className='highlight'>la conception globale</span> de solutions
+          performantes. <br />
           <br />
-          <br />
-          Convaincue qu&apos;une bonne architecture doit servir à la rentabilité et à la stratégie, je souhaite préparer un 
-          <span className='highlight'> {' '}Bac+5 en Ingénierie logicielle</span>{' '}
-          en alternance. Mon objectif est de concevoir des solutions scalables, valoriser vos données et aligner la technique sur vos{' '}
+          Convaincue que la technique doit servir la stratégie, je souhaite
+          préparer un{' '}
+          <span className='highlight'>
+            Bac+5 en Ingénierie logicielle ou Data Engineer
+          </span>{' '}
+          en alternance. Mon objectif est de bâtir des systèmes scalables, de{' '}
+          <span className='highlight'>valoriser vos données</span> et
+          d&apos;aligner chaque brique logicielle sur vos{' '}
           <span className='highlight'>enjeux business.</span>
         </p>
       </div>
